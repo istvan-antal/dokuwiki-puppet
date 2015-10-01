1ce712b639348a2af5a5c9d2134eae3d12e23d4f
István Antal <istvan.m.antal@gmail.com>
2015-10-01 13:28:21 +0300

    Refined user groups

--- modules/main/files/users.auth.php
+++ modules/main/files/users.auth.php
@@ -1,3 +1,3 @@
 # users.auth.php
 # <?php exit()?>
-root:c02a0e0a263aac5bdf8effbc18d2d81e540f9864:Admin:admin@example.com:admins
+root:c02a0e0a263aac5bdf8effbc18d2d81e540f9864:Admin:admin@example.com:admins,managers,users
