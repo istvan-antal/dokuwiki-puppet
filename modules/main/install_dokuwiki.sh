493128909d45982410ac2f05afd22511c7bae689
István Antal <istvan.m.antal@gmail.com>
2015-10-01 13:30:54 +0300

    Use our default config when first setting up dokuwiki

--- modules/main/install_dokuwiki.sh
+++ modules/main/install_dokuwiki.sh
@@ -3,4 +3,5 @@ wget http://download.dokuwiki.org/src/dokuwiki/dokuwiki-$1.tgz
 tar xvf dokuwiki-$1.tgz
 rm dokuwiki-$1.tgz
 mv dokuwiki-$1 web
+rm web/conf/dokuwiki.php # Use our default config instead
 mv web/data .
